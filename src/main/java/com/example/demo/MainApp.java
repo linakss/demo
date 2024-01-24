ec1c9aa5c953bb7a8ee270ea63edc977378fdff0
linakss <linbuster444@gmail.com>
2024-01-24 16:34:56 +0300

    всё

---
+++ src/main/java/com/example/demo/MainApp.java
@@ -0,0 +1,152 @@
+package com.example.demo;
+
+import com.example.demo.controller.PersonEditDialogController;
+import com.example.demo.controller.PersonOverviewController;
+import com.example.demo.model.Person;
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class MainApp extends Application {
+    private Stage primaryStage;
+    private BorderPane rootLayout;
+
+    /**
+     * Данные, в виде наблюдаемого списка адресатов.
+     */
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    /**
+     * Конструктор
+     */
+    public MainApp() {
+        // В качестве образца добавляем некоторые данные
+        personData.add(new Person("Илья","Пушин"));
+        personData.add(new Person("Илья","Кияшко"));
+        personData.add(new Person("Илья","Крапивин"));
+        personData.add(new Person("Данил","Степаненко"));
+        personData.add(new Person("Никита","Тубальцев"));
+        personData.add(new Person("Каролина","Соцкая-Сотникова"));
+        personData.add(new Person("Вадим","Слесенко"));
+        personData.add(new Person("Игорь","Кузнецов"));
+        personData.add(new Person("Лягух","Лягухович"));
+    }
+    /**
+     * Возвращает данные в виде наблюдаемого списка адресатов.
+     * @return
+     */
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("AddressApp");
+        this.primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("images/address_book_icon.png")));
+        initRootLayout();
+
+        showPersonOverview();
+    }
+
+    /**
+     * Инициализирует корневой макет.
+     */
+    public void initRootLayout() {
+        try {
+            // Загружаем корневой макет из fxml файла.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
+
+            // Отображаем сцену, содержащую корневой макет.
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Показывает в корневом макете сведения об адресатах.
+     */
+    public void showPersonOverview() {
+        try {
+            // Загружаем сведения об адресатах.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            // Помещаем сведения об адресатах в центр корневого макета.
+            rootLayout.setCenter(personOverview);
+
+            // Даём контроллеру доступ к главному приложению.
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Открывает диалоговое окно для изменения деталей указанного адресата.
+     * Если пользователь кликнул OK, то изменения сохраняются в предоставленном
+     * объекте адресата и возвращается значение true.
+     *
+     * @param person - объект адресата, который надо изменить
+     * @return true, если пользователь кликнул OK, в противном случае false.
+     */
+    public boolean showPersonEditDialog(Person person) {
+        try {
+            // Загружаем fxml-файл и создаём новую сцену
+            // для всплывающего диалогового окна.
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonEditDialog.fxml"));
+            AnchorPane page = (AnchorPane) loader.load();
+
+            // Создаём диалоговое окно Stage.
+            Stage dialogStage = new Stage();
+            dialogStage.setTitle("Edit Person");
+            dialogStage.initModality(Modality.WINDOW_MODAL);
+            dialogStage.initOwner(primaryStage);
+            Scene scene = new Scene(page);
+            dialogStage.setScene(scene);
+
+            // Передаём адресата в контроллер.
+            PersonEditDialogController controller = loader.getController();
+            controller.setDialogStage(dialogStage);
+            controller.setPerson(person);
+
+            // Отображаем диалоговое окно и ждём, пока пользователь его не закроет
+            dialogStage.showAndWait();
+
+            return controller.isOkClicked();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    /**
+     * Возвращает главную сцену.
+     * @return
+     */
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/example/demo/util/DateUtil.java
@@ -0,0 +1,52 @@
+package com.example.demo.util;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+public class DateUtil {
+    /** Шаблон даты, используемый для преобразования. Можно поменять на свой. */
+    private static final String DATE_PATTERN = "dd.MM.yyyy";
+    /** Форматировщик даты. */
+    private static final DateTimeFormatter DATE_FORMATTER =
+            DateTimeFormatter.ofPattern(DATE_PATTERN);
+    /**
+     * Возвращает полученную дату в виде хорошо отформатированной строки.
+     * Используется определённый выше {@link DateUtil#DATE_PATTERN}.
+     *
+     * @param date - дата, которая будет возвращена в виде строки
+     * @return отформатированную строку
+     */
+    public static String format(LocalDate date) {
+        if (date == null) {
+            return null;
+        }
+        return DATE_FORMATTER.format(date);
+    }
+    /**
+     * Преобразует строку, которая отформатирована по правилам
+     * шаблона {@linkDateUtil#DATE_PATTERN} в объект {@linkLocalDate}.
+     *
+     * Возвращает null, если строка не может быть преобразована.
+     *
+     * @paramdateString - дата в виде String
+     * @return объект даты или null, если строка не может быть преобразована
+     */
+    public static LocalDate parse(String dateString) {
+        try {
+            return DATE_FORMATTER.parse(dateString, LocalDate::from);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
+    }
+    /**
+     * Проверяет, является ли строка корректной датой.
+     *
+     * @paramdateString
+     * @return true, если строка является корректной датой
+     */
+    public static boolean validDate(String dateString) {
+        // Пытаемся разобрать строку.
+        return DateUtil.parse(dateString) != null;
+    }
+}
